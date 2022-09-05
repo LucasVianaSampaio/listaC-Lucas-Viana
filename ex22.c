1decb7b6bc4c4f0525bb4680ec9360dc818a1adf
LucasVianaSampaio <lv6612530@gmail.com>
2022-09-05 15:55:52 -0300

    Questão 22

---
+++ ex22.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+  char letraA = 'A';
+  char letraB = 'B';
+  char letraC = 'C';
+
+  char letraa = 'a';
+  char letrab = 'b';
+  char letrac = 'c';
+
+  char num0 = '0';
+  char num1 = '1';
+  char num2 = '2';
+
+  char simb1 = '$';
+  char simb2 = '*';
+  char simb3 = '+';
+  char simb4 = '/';
+  char simb5 = ' ';
+
+  printf("O inteiro equivalente a letra A é: %d\n", letraA);
+  printf("O inteiro equivalente a letra B é: %d\n", letraB);
+  printf("O inteiro equivalente a letra B é: %d\n", letraC);
+
+  printf("O inteiro equivalente a letra a é: %d\n", letraa);
+  printf("O inteiro equivalente a letra b é: %d\n", letrab);
+  printf("O inteiro equivalente a letra c é: %d\n", letrac);
+
+  printf("O inteiro equivalente ao número 0 é: %d\n", num0);
+  printf("O inteiro equivalente ao número 1 é: %d\n", num1);
+  printf("O inteiro equivalente ao número 2 é: %d\n", num2);
+
+  printf("O inteiro equivalente ao símbolo $ é: %d\n", simb1);
+  printf("O inteiro equivalente ao símbolo * é: %d\n", simb2);
+  printf("O inteiro equivalente ao símbolo + é: %d\n", simb3);
+  printf("O inteiro equivalente ao símbolo / é: %d\n", simb4);
+  printf("O inteiro equivalente ao símbolo (espaço em branco) é: %d\n", simb5);
+
+  return 0;
+}
